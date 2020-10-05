e521d3bfdc6b0b797e51a38f7195ff5c400fb17d
CD-prog <61663007+CD-prog@users.noreply.github.com>
2020-10-05 16:13:26 -0500

    app is now responsive; fixed some display issues

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,21 +1,16 @@
 import React from 'react'
-import {Navbar, Form, FormControl, Button} from 'react-bootstrap'
+import { Navbar, Form, FormControl, Button } from 'react-bootstrap'
 
 function Search() {
   return (
-      <>
+    <>
       <Navbar bg="light" expand="lg">
-  
- 
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
- 
-</Navbar>
-    
-      
-      </>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar>
+    </>
   )
 }
 export default Search
