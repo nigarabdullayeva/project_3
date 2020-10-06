2afa64cb82053a2db0936b24f6ad35d8f5d75d37
CD-prog <61663007+CD-prog@users.noreply.github.com>
2020-10-06 08:55:03 -0500

    installed sass, renamed index.scss

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import{Form,Button} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -13,12 +12,12 @@ function Login() {
       <br/>
       <br/>
     <div className="form-group">
-      <label for="exampleInputEmail1">Email address</label>
+      <label htmlFor="exampleInputEmail1">Email address</label>
       <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
       <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
     <div className="form-group">
-      <label for="exampleInputPassword1">Password</label>
+      <label htmlFor="exampleInputPassword1">Password</label>
       <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
     </div>
     
