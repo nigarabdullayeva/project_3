6c50d17e568f141a3fbe8c319cc18b962e804482
CD-prog <61663007+CD-prog@users.noreply.github.com>
2020-10-09 10:37:14 -0500

    removed node_modules

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -5,9 +5,10 @@ import User from '../components/User'
 const Profile = ({ handleLogout }) => {
   return (
     <>
+        <br/>
+      <button className="btn-primary" onClick={handleLogout}>Logout</button>
       <h1>User Profile Page</h1>
       <User/>
-      <button className="btn-primary" onClick={handleLogout}>Logout</button>
     </>
   )
 }
