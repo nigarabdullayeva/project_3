660a72cc82a4f9a4630102746abb72eb65bac424
CD-prog <61663007+CD-prog@users.noreply.github.com>
2020-10-10 14:21:06 -0500

    back end working

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,28 +1,99 @@
-import React from 'react'
+import React, { useState } from 'react'
 import API from "../utils/API";
 
-
-
 function Item() {
-  //API.saveItemTest();
-  console.log(API.saveItemTest());
-  
+  const [item, setItem] = useState({
+    title: "",
+    description: "",
+    location: "",
+    price: "",
+    photoURL: "",
+    userName: "",
+    phone: ""
+  })
 
+  const handleSetItem = (e) => {
+    console.log('we r typing!!', e.target.name)
+    setItem({
+      ...item,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSaveItem = (e) => {
+    e.preventDefault();
+    API.saveItem(item);
+  };
 
   return (
-      <>
-      
-    <form>
-     <div className="form-group">
-    <label for="exampleInputEmail1">Item title </label>
-    <input type="text" className="form-control"/>
-    <div className="form-group">
-    <label for="exampleInputPassword1">Description</label>
-    <input type="text" className="form-control" />
-    </div>
-    <button type="submit" className="btn btn-primary">Submit</button>
-    </div>
-    </form>     
+    <>
+
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Item title </label>
+          <input
+            type="text"
+            className="form-control"
+            name="title"
+            onChange={handleSetItem}
+          />
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Description</label>
+            <input
+              type="text"
+              className="form-control"
+              name="description"
+              onChange={handleSetItem}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Location</label>
+            <input
+              type="text"
+              className="form-control"
+              name="location"
+              onChange={handleSetItem}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Price</label>
+            <input
+              type="text"
+              className="form-control"
+              name="price"
+              onChange={handleSetItem}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Photo</label>
+            <input
+              type="text"
+              className="form-control"
+              name="photoURL"
+              onChange={handleSetItem}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Username</label>
+            <input
+              type="text"
+              className="form-control"
+              name="userName"
+              onChange={handleSetItem}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Phone</label>
+            <input
+              type="text"
+              className="form-control"
+              name="phone"
+              onChange={handleSetItem}
+            />
+          </div>
+          <button onClick={handleSaveItem} type="submit" className="btn btn-primary">Submit</button>
+        </div>
+      </form>
     </>
   )
 
