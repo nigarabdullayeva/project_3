414ffe11d5fedae8128c644121fcf024ef459e9c
CD-prog <61663007+CD-prog@users.noreply.github.com>
2020-10-16 17:34:42 -0500

    cleanup

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import User from '../components/User'
 import History from '../components/History'
-import '../containers/Profile.css'  
+import '../containers/Profile.css' 
+ 
 const Profile = ({ handleLogout, user }) => {
 
   return (
     <>
       <br/>
-      <p>Welcome, {user.email}</p>
+      <p>You are currently signed in as : {user.email}</p>
       <button className="btn-primary" onClick={handleLogout}>Logout</button>
       <br/>
       <User/>
