e80c2b0405e67bbd92906a1df6ca513d6a5cd5b6
CD-prog <61663007+CD-prog@users.noreply.github.com>
2020-10-18 12:52:17 -0500

    button working for making items available

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,33 +1,33 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import User from '../components/User'
-import '../containers/Profile.css' 
+import '../containers/Profile.css'
 import axios from 'axios';
 
- 
+
 const Profile = ({ handleLogout, user }) => {
   const [results, setResults] = useState([]);
-  useEffect(()=>{
-  const itemsForRent = () => {
-     axios.get('/api/items/myitems/all', {
-        params:{email: user.email}
+  useEffect(() => {
+    const itemsForRent = () => {
+      axios.get('/api/items/myitems/all', {
+        params: { email: user.email }
       })
-      .then(({ data }) =>setResults(data)) 
-      .catch((err)=>{console.log(err)})  
+        .then(({ data }) => setResults(data))
+        .catch((err) => { console.log(err) })
     }
-      itemsForRent()
-    },[user]) 
-    
+    itemsForRent()
+  }, [user])
+
   return (
     <>
-      <br/>
+      <br />
       <h3>You are currently signed in as : {user.email}</h3>
       <button className="btn-primary" onClick={handleLogout}>Logout</button>
-      <br/>
-      <User/>
-      
+      <br />
+      <User />
+
       {results.length ? results.map((result) => <div className="slide-in-bottom card float-right w-25" key={result._id}>
-          {console.log(result)}
-          <div className="card-body">
+        {/* {console.log(result)} */}
+        <div className="card-body">
           <h5 className="card-header text-center">{result.title}</h5>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">Category: {result.category}</li>
@@ -38,21 +38,21 @@ const Profile = ({ handleLogout, user }) => {
             <li className="list-group-item">Email: {result.email}</li>
             <li className="list-group-item">RentedBy: {result.rentedBy}</li>
           </ul>
-          {console.log(result._id)}
-          
-          <button
-            onClick={(id) => {
-              id=result._id;
-              axios.put('/api/items/'+id,{rentedBy: null })
-                .then(({ data }) => data)
-                .catch((err) => { console.log(err) })
-            }
-            }
-            className="card-link">
-            Make item available
-         </button>
+          {/* {console.log(result._id)} */}
 
-
+          {(result.rentedBy != null) ?
+            <button
+              onClick={(id) => {
+                id = result._id;
+                axios.put('/api/items/' + id, { rentedBy: null })
+                  .then(({ data }) => data)
+                  .catch((err) => { console.log(err) })
+              }
+              }
+              className="card-link">
+              Make item available
+         </button> : null
+          }
         </div>
       </div>) : null}
 
